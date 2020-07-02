881145ee7d4ae7d45288c8d91b7cefec703a1b02
Andrey Mashukov <andre.mashukov@gmail.com>
2020-07-02 15:33:47 +0800

    longest increasing path in the matrix

---
+++ test/lip/longestIncreasingPath.test.js
@@ -0,0 +1,13 @@
+const lip = require('../../src/lip/longestIncreasingPath');
+
+describe('longestIncreasingPath', () => {
+  it('should find the length of the longest increasing path', () => {
+    // The longest increasing path
+    // is [1, 2, 6, 9]
+    expect(lip.longestIncreasingPath([
+      [9, 9, 4],
+      [6, 6, 8],
+      [2, 1, 1],
+    ])).toBe(4);
+  });
+});
