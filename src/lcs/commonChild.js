b5eb5f309d4c6f6c9d67f71d67ee2db658b72700
Andrey Mashukov <andre.mashukov@gmail.com>
2020-07-02 15:17:33 +0800

    Reusing LCS for commonChild

--- src/lcs/commonChild.js
+++ src/lcs/commonChild.js
@@ -1,3 +1,4 @@
+const lcs = require('./longestCommonSubsequence');
 // A string is said to be a child of a another string
 // if it can be formed by deleting 0 or more characters
 // from the other string. Given two strings of equal length,
@@ -5,9 +6,9 @@
 // such that it is a child of both?
 
 /**
- * @param {string[]} s1
- * @param {string[]} s2
- * @return {string[]}
+ * @param {string} s1
+ * @param {string} s2
+ * @return {number}
  */
 function commonChild(s1, s2) {
   const arr = [Array(s2.length + 1).fill(0)];
@@ -21,4 +22,14 @@ function commonChild(s1, s2) {
   return arr[s2.length][s1.length];
 }
 
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {string}
+ */
+function commonChildLcs(s1, s2) {
+  return lcs.longestCommonSubsequence([...s1], [...s2]).length;
+}
+
 module.exports.commonChild = commonChild;
+module.exports.commonChildLcs = commonChildLcs;
--- test/lcs/commonChild.test.js
+++ test/lcs/commonChild.test.js
@@ -1,6 +1,6 @@
 const cc = require('../../src/lcs/commonChild');
 
-describe('commonChild test suite', () => {
+describe('commonChild', () => {
   it('HARRY and SALLY => AY of length 2', async () => {
     expect(
         cc.commonChild('HARRY', 'SALLY'))
@@ -10,6 +10,20 @@ describe('commonChild test suite', () => {
   it('SHINCHAN and SALLY => NHA of length 3', async () => {
     expect(
         cc.commonChild('SHINCHAN', 'NOHARAAA'))
+        .toEqual(3);
+  });
+});
+
+describe('commonChild reusing LCS', () => {
+  it('HARRY and SALLY => AY of length 2', async () => {
+    expect(
+        cc.commonChildLcs('HARRY', 'SALLY'))
+        .toEqual(2);
+  });
+
+  it('SHINCHAN and SALLY => NHA of length 3', async () => {
+    expect(
+        cc.commonChildLcs('SHINCHAN', 'NOHARAAA'))
         .toEqual(3);
   });
 });
