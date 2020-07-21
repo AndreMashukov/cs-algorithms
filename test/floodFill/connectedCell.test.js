d6fc919979e87df8ccb42416c90d911b89401400
Andrey Mashukov <andre.mashukov@gmail.com>
2020-07-21 19:10:06 +0800

    Added test for connected cell

--- test/floodFill/connectedCell.test.js
+++ test/floodFill/connectedCell.test.js
@@ -12,6 +12,15 @@ describe('DFS: connectedCell', () => {
 
   it('should return 5', async () => {
     expect(cc.connectedCell([
+      [1, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 0, 1, 0],
+      [1, 0, 0, 0],
+    ])).toEqual(4);
+  });
+
+  it('should return 5', async () => {
+    expect(cc.connectedCell([
       [1, 1, 0, 0, 0],
       [0, 1, 1, 0, 0],
       [0, 0, 1, 0, 1],
