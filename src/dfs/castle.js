2f4b667565d17a9132f7fc3e91ffbf78c06b2efc
Andrey Mashukov <andre.mashukov@gmail.com>
2020-07-23 13:51:47 +0800

    Refactor castle.js

--- src/dfs/castle.js
+++ src/dfs/castle.js
@@ -29,8 +29,9 @@ function minimumMoves(grid, startRow, startCol, endRow, endCol) {
   const goalY = endCol;
 
   const n = grid.length;
-  game = Array(n).fill(0).map((x) => Array(n).fill(0));
-  helper = Array(n).fill(0).map((x) => Array(n).fill(0));
+  const game = Array(n).fill(0).map((x) => Array(n).fill(0));
+  // store 1 if that node has been visited, else 0.
+  const visited = Array(n).fill(0).map((x) => Array(n).fill(0));
   for (let i=0; i<n; i++) {
     for (let j=0; j<n; j++) {
       if (grid[i].charAt(j) === '.') {
@@ -47,52 +48,55 @@ function minimumMoves(grid, startRow, startCol, endRow, endCol) {
 
   while (move.length > 0) {
     const current = move.pop();
-    if (helper[current.x][current.y] === 0) {
-      helper[current.x][current.y] = 1;
+    if (visited[current.x][current.y] === 0) {
+      visited[current.x][current.y] = 1;
       moveGenerator(current, grid.length, move);
     }
   }
   return game[goalX][goalY];
-}
 
-/**
+  /**
  * Add a new Point to move array
  * @param {Point} p -point
  * @param {number} n - grid.length
  * @param {array} _move - array of points
  */
-function moveGenerator(p, n, _move) {
-  const x = p.x;
-  const y = p.y;
-  const value = game[x][y];
-  for (let i=x; i<n && game[i][y]!=-1; i++) {
-    addStep(i, y, value);
-    _move.push(new Point(i, y));
+  function moveGenerator(p, n, _move) {
+    const x = p.x;
+    const y = p.y;
+    const value = game[x][y];
+    // Iterate from p horizontaly until reach border.
+    for (let i=x; i<n && game[i][y]!=-1; i++) {
+      addStep(i, y, value);
+      _move.push(new Point(i, y));
+    }
+    // Iterate from p horizontaly until reach blocked cell.
+    for (let i=x; i>=0 && game[i][y]!=-1; i--) {
+      addStep(i, y, value);
+      _move.push(new Point(i, y));
+    }
+    // Iterate from p vertically -> border.
+    for (let i=y; i<n && game[x][i]!=-1; i++) {
+      addStep(x, i, value);
+      _move.push(new Point(x, i));
+    }
+    // Iterate from p vertically -> blocked cell.
+    for (let i=y; i>=0 && game[x][i]!=-1; i--) {
+      addStep(x, i, value);
+      _move.push(new Point(x, i));
+    }
   }
-  for (let i=x; i>=0 && game[i][y]!=-1; i--) {
-    addStep(i, y, value);
-    _move.push(new Point(i, y));
-  }
-  for (let i=y; i<n && game[x][i]!=-1; i++) {
-    addStep(x, i, value);
-    _move.push(new Point(x, i));
-  }
-  for (let i=y; i>=0 && game[x][i]!=-1; i--) {
-    addStep(x, i, value);
-    _move.push(new Point(x, i));
-  }
-}
 
-/**
+  /**
  * Puts the number of moves on the grid
  * @param {number} x - x
  * @param {number} y - y
  * @param {number} value - number of moves made.
  */
-function addStep(x, y, value) {
-  // console.log(game);
-  if (game[x][y] > value+1) {
-    game[x][y] = value+1;
+  function addStep(x, y, value) {
+    if (game[x][y] > value+1) {
+      game[x][y] = value+1;
+    }
   }
 }
 
@@ -105,3 +109,23 @@ module.exports.minimumMoves = minimumMoves;
 // Given a grid, a start and an end position, determine the number
 // of moves it will take to get to the end position.
 
+// Fill all the positions in the grid with the number
+// of moves it takes to rech there.
+// Populate the whole grid and then index
+// to the (goalX, goalY) to find the answer.
+
+// '.X.',
+// '.X.',
+// '...',
+// game:
+// [ [ 100, -1, 100 ],
+//   [ 100, -1, 100 ],
+//   [ 100, 100, 100 ] ]
+// helper:
+// [ [ 1, 0, 1 ],
+//   [ 1, 0, 1 ],
+//   [ 1, 1, 1 ] ]
+// game:
+// [ [ 0, -1, 3 ],
+//   [ 1, -1, 3 ],
+//   [ 1,  2, 2 ] ]
