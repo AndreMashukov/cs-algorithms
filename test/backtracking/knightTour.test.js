f1703f199a1c9f3fcf5cc87f03028f8802b90eef
Andrey Mashukov <andre.mashukov@gmail.com>
2022-07-27 15:42:51 +0800

    Backtracking Knight Tour: added description

--- test/backtracking/knightTour.test.js
+++ test/backtracking/knightTour.test.js
@@ -11,7 +11,13 @@ describe('knightTour', () => {
       [42, 57, 2, 49, 40, 23, 6, 19],
       [47, 50, 45, 54, 25, 20, 11, 14],
       [56, 43, 52, 3, 22, 13, 24, 5],
-      [51, 46, 55, 44, 53, 4, 21, 12],
+      [51, 46, 55, 44, 53, 4, 21, 12]
     ]);
   });
 });
+
+// https://www.geeksforgeeks.org/the-knights-tour-problem-backtracking-1/
+
+// Given a N*N board with the Knight placed on the first block of an empty board.
+// Moving according to the rules of chess knight must visit each square exactly once.
+// Print the order of each cell in which they are visited.
