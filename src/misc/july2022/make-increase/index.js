fccc1df43799a686dcc34944105f8d7d0f11c1d2
Andrey Mashukov <nilesh@refreshdigital.com.au>
2023-02-28 21:13:56 +0800

    dim/-gaai/ mouh/ yahn\ gong/

--- src/misc/july2022/make-increase/index.js
+++ src/misc/july2022/make-increase/index.js
@@ -1,63 +1,70 @@
 const checkIfStrictIncr = (list) => {
-  let result = list[0];
+  let result = list[0]
   for (let i = 1; i < list.length; i++) {
     if (list[i] > result) {
-      result = list[i];
+      result = list[i]
     } else {
-      return false;
+      return false
     }
   }
-  return true;
-};
+  return true
+}
 
 const swap = (number, target) => {
+  // number = 900, target = 10
   const array = `${number}`
     .split('')
     .map((n) => parseInt(n, 10))
     .sort((a, b) => {
       if (a > b) {
-        return 1;
+        return 1
       }
       if (a < b) {
-        return -1;
+        return -1
       }
       if (a === b) {
-        return 0;
+        return 0
       }
-    });
+    })
+  // array = [0, 0, 9]
   if (array.length === 1) {
-    return [number];
+    return [number]
   } else {
     const reversed = array
       .map((item) => `${item}`)
       .reverse()
-      .map((item) => parseInt(item, 10));
-    const max = parseInt(reversed.join(''), 10);
-    const min = parseInt(array.join(''), 10);
-    // console.log({ array, max, min, reversed });
-    return [min, max].filter((item) => item < target);
+      .map((item) => parseInt(item, 10))
+    // reversed = [9, 0, 0]
+    const max = parseInt(reversed.join(''), 10) // 900
+    const min = parseInt(array.join(''), 10) // 9
+    // console.log({ array, max, min, reversed })
+    // only values that less than target which is next element
+    return [min, max].filter((item) => item < target)
   }
-};
+}
 
 const naive = (numbers) => {
   if (checkIfStrictIncr(numbers)) {
-    return true;
+    return true
   } else {
     for (let i = 0; i < numbers.length; i++) {
-      const copy = [...numbers];
+      const copy = [...numbers]
+      // only for 2 digit and higher numbers
       if (copy[i] > 9) {
-        const perms = swap(copy[i], numbers[i + 1]);
-
+        // check if next current number has permutations
+        // which are less then its next number
+        const perms = swap(copy[i], numbers[i + 1])
+        console.log({ perms })
         for (const p of perms) {
-          copy[i] = p;
-          // console.log(copy);
+          copy[i] = p
+          console.log(copy)
           if (checkIfStrictIncr(copy)) {
-            return true;
+            return true
           }
         }
       }
     }
-    return false;
+    return false
   }
-};
-module.exports.makeIncrease = { naive };
+}
+module.exports.makeIncrease = { naive }
--- test/misc/july2022/make-increase.test.js
+++ test/misc/july2022/make-increase.test.js
@@ -1,20 +1,20 @@
-const { makeIncrease } = require('../../../src/misc/july2022/make-increase');
+const { makeIncrease } = require('../../../src/misc/july2022/make-increase')
 
 describe('misc.july2022.make-increase', () => {
   it('should return true', () => {
-    const result = makeIncrease.naive([1, 5, 10, 20]);
-    expect(result).toEqual(true);
-  });
+    const result = makeIncrease.naive([1, 5, 10, 20])
+    expect(result).toEqual(true)
+  })
   it('should return true', () => {
-    const result = makeIncrease.naive([1, 3, 900, 10]);
-    expect(result).toEqual(true);
-  });
+    const result = makeIncrease.naive([1, 3, 900, 10])
+    expect(result).toEqual(true)
+  })
 
   it('should return true', () => {
-    const result = makeIncrease.naive([13, 31, 30]);
-    expect(result).toEqual(false);
-  });
-});
+    const result = makeIncrease.naive([13, 31, 30])
+    expect(result).toEqual(false)
+  })
+})
 
 // https://leetcode.com/discuss/interview-question/1528492/codesignal-makeincreasing#:~:text=You%20are%20allowed%20to%20choose,will%20be%20considered%20just%2010).
 
