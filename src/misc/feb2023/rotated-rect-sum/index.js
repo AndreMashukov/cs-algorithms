2f8164d7087affbfd0f28f65c0d7bf935697ccf3
Andrey Mashukov <andre.mashukov@gmail.com>
2023-03-03 18:58:00 +0800

    Guòlǜ. 過濾. filter

--- src/misc/feb2023/rotated-rect-sum/index.js
+++ src/misc/feb2023/rotated-rect-sum/index.js
@@ -2,47 +2,54 @@
 
 const getLength = (matrix) => {
   // rows, cols = len(matrix), len(matrix[0])
-  return { rowLength: matrix.length, colLength: matrix[0].length }
-}
+  return {rowLength: matrix.length, colLength: matrix[0].length};
+};
 
-const getPrimaryDiagonal = (matrix, anchor) => {
-  const { rowLength, colLength } = getLength(matrix)
-  const diag = []
-  for (let i = 0; i < rowLength; i++) {
-    for (let j = 0; j < colLength; j++) {
-      if (i === j) {
-        if (matrix[i + anchor]) {
-          diag.push(matrix[i + anchor][j])
+const solution = (matrix, a, b) => {
+  let maxArea = -Infinity;
+  const {rowLength: rows, colLength: cols} = getLength(matrix);
+  const ab = [
+    [a, b],
+    [b, a],
+  ];
+    // go through possible rectangles along both diagonals
+  for (const [w, h] of ab ) {
+    // go through possible "anchors", which is the left top coordinate
+    // of the rectangle candidate
+    // console.log({p1: rows-h+1, p2: cols - (h + w -1) + 1});
+    for (let i = w-1; i < rows - h + 1; i++) {
+      for (let j = 0; j < cols - (h + w - 1) + 1; j++) {
+        let area = 0;
+        // console.log({w, h, i, j});
+
+        // sum up the long diagonals
+        for (let p = 0; p < w; p++) { // go to next long diagonal
+          for (let q = 0; q < h; q++) { // go down current diagonal
+            area += matrix[i - p + q][j + p + q];
+            // console.log({m: matrix[i - p + q][j + p + q]});
+          }
+        }
+
+        // sum up the short diagonals
+        // note that short diagonals have one less element than long diagonals
+        const k = i;
+        const l = j + 1;
+
+        for (let p = 0; p < w - 1; p++) { // go to next long diagonal
+          for (let q = 0; q < h - 1; q++) { // go down current diagonal
+            area += matrix[k - p + q][l + p + q];
+            // console.log({m: matrix[i - p + q][j + p + q]});
+          }
+        }
+
+        if (area > maxArea) {
+          maxArea = area;
         }
       }
     }
   }
-  // console.log({ rowLength, colLength })
-  return diag
-}
+  return maxArea;
+};
 
-const getSecondaryDiagonal = (matrix) => {
-  const { rowLength, colLength } = getLength(matrix)
-  const diag = []
-  for (let i = 0; i < rowLength; i++) {
-    for (let j = 0; j < colLength; j++) {
-      if (i === rowLength - j) {
-        diag.push(matrix[i][j])
-      }
-    }
-  }
-  // console.log({ rowLength, colLength })
-  return diag
-}
+module.exports.rotatedRectSum = {solution};
 
-const solution = (matrix, a, b) => {
-  const { rowLength, colLength } = getLength(matrix)
-  const primeDiagList = []
-  for (let i = 0; i < rowLength; i++) {
-    primeDiagList.push(getPrimaryDiagonal(matrix, i))
-  }
-  console.log({ sec: getSecondaryDiagonal(matrix) })
-  return 1
-}
-
-module.exports.rotatedRectSum = { solution }
--- test/misc/feb2023/rotated-rect-sum.test.js
+++ test/misc/feb2023/rotated-rect-sum.test.js
@@ -1,4 +1,4 @@
-const { rotatedRectSum } = require('../../../src/misc/feb2023/rotated-rect-sum')
+const {rotatedRectSum} = require('../../../src/misc/feb2023/rotated-rect-sum');
 
 describe('misc.feb2023.rotated-rect-sum', () => {
   it('should return 36', () => {
@@ -6,32 +6,32 @@ describe('misc.feb2023.rotated-rect-sum', () => {
       [1, 2, 3, 4, 0],
       [5, 6, 7, 8, 1],
       [3, 2, 4, 1, 4],
-      [4, 3, 5, 1, 6]
-    ]
+      [4, 3, 5, 1, 6],
+    ];
 
-    const result = rotatedRectSum.solution(matrix, 2, 3)
-    expect(result).toEqual(36)
-  })
+    const result = rotatedRectSum.solution(matrix, 2, 3);
+    expect(result).toEqual(36);
+  });
 
   it('should return 7', () => {
     const matrix = [
       [-2, 3],
-      [4, 3]
-    ]
+      [4, 3],
+    ];
 
-    const result = rotatedRectSum.solution(matrix, 1, 2)
-    expect(result).toEqual(7)
-  })
+    const result = rotatedRectSum.solution(matrix, 1, 2);
+    expect(result).toEqual(7);
+  });
 
   it('should return 10', () => {
     const matrix = [
       [-2, 3, 5, -1],
-      [4, 3, -10, 10]
-    ]
+      [4, 3, -10, 10],
+    ];
 
-    const result = rotatedRectSum.solution(matrix, 1, 1)
-    expect(result).toEqual(10)
-  })
-})
+    const result = rotatedRectSum.solution(matrix, 1, 1);
+    expect(result).toEqual(10);
+  });
+});
 
 // https://leetcode.com/discuss/interview-question/1482150/rotatedrectsum-codesignal-quora-two-sigma-oa
