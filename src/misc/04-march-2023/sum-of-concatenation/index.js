3ed08574e818fa6290a92af8ace14d9a06a32c82
Andrey Mashukov <nilesh@refreshdigital.com.au>
2023-03-19 17:12:31 +0800

    我以為好平，原来咁貴

--- src/misc/04-march-2023/sum-of-concatenation/index.js
+++ src/misc/04-march-2023/sum-of-concatenation/index.js
@@ -1,20 +1,20 @@
 const solution = (a) => {
-  let lowSum = 0;
+  let lowSum = 0
 
   for (let i = 0; i < a.length; i++) {
-    lowSum += a[i];
+    lowSum += a[i]
   }
 
-  let cnt = lowSum * a.length; // 12 * 2 = 24
+  let cnt = lowSum * a.length // 12 * 2 = 24
 
   for (let j = 0; j < a.length; j++) {
-    const size = `${a[j]}`.length;
-    const offset = Math.pow(10, size);
-    cnt += lowSum * offset;
-    console.log({aj: a[j], size, offset, cnt});
+    const size = `${a[j]}`.length
+    const offset = Math.pow(10, size)
+    cnt += lowSum * offset
+    console.log({ aj: a[j], size, offset, cnt })
   }
 
-  return cnt;
-};
+  return cnt
+}
 
-module.exports.sumOfConcatenation = {solution};
+module.exports.sumOfConcatenation = { solution }
