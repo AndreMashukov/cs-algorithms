17a4bae5391fa56f1ad66a752c98e90870e7578b
Andrey Mashukov <nilesh@refreshdigital.com.au>
2023-04-02 11:55:38 +0800

    依靠

--- src/misc/04-march-2023/contain-pair/index.js
+++ src/misc/04-march-2023/contain-pair/index.js
@@ -1,29 +1,41 @@
-const solution = () => {
-  const num_subarray_with_sum_k = 0
+const solution = (a, m, k) => {
+  let num_subarray_with_sum_k = 0
   const cnt_in_subarray = {}
   const idx_subarray = {}
-  const last_index = -1
+  let last_index = -1
 
-
-  // for i in range(m):
-  // if a[i] in idx_subarray:
-  //     num_subarray_with_sum_k = 1
-  //     last_index = idx_subarray[a[i]]    
-  // cnt_in_subarray[a[i]] += 1
-  // idx_subarray[k-a[i]] = i
-
-  for (let i = 0; i < m; i < m; i++) {
+  for (let i = 0; i < m; i++) {
     if (idx_subarray[a[i]]) {
-      /// ????
+      num_subarray_with_sum_k = 1
+      last_index = idx_subarray[a[i]]
     }
+    cnt_in_subarray[a[i]] += 1
+    idx_subarray[k - a[i]] = i
   }
 
+  // console.log({ idx_subarray })
 
-  return 5
+  for (let i = m; i < a.length; i++) {
+    cnt_in_subarray[a[i - m]] -= 1
+    if (cnt_in_subarray[a[i - m]] === 0) {
+      delete idx_subarray[k - a[i - m]]
+    }
+
+    if (idx_subarray[a[i]]) {
+      num_subarray_with_sum_k += 1
+      last_index = Math.max(idx_subarray[a[i]], last_index)
+    } else if (last_index > i - m) {
+      num_subarray_with_sum_k += 1
+    }
+
+    cnt_in_subarray[a[i]] += 1
+    idx_subarray[k - a[i]] = i
+  }
+
+  return num_subarray_with_sum_k
 }
 
 module.exports.containPair = { solution }
-
 
 // def add_to_k(a: List[int], m: int, k: int):
 //     num_subarray_with_sum_k = 0
@@ -34,10 +46,10 @@ module.exports.containPair = { solution }
 //     for i in range(m):
 //         if a[i] in idx_subarray:
 //             num_subarray_with_sum_k = 1
-//             last_index = idx_subarray[a[i]]    
+//             last_index = idx_subarray[a[i]]
 //         cnt_in_subarray[a[i]] += 1
 //         idx_subarray[k-a[i]] = i
-     
+
 //     for i in range(m, len(a)):
 //         cnt_in_subarray[a[i-m]] -= 1
 //         if cnt_in_subarray[a[i-m]] == 0:
