1eab659175e8fb511e15a869489b9e93882e5b02
Andrey Mashukov <nilesh@refreshdigital.com.au>
2023-04-12 13:05:00 +0800

    jobs-build-steps

--- src/misc/04-march-2023/contain-pair/index.js
+++ src/misc/04-march-2023/contain-pair/index.js
@@ -9,25 +9,40 @@ const solution = (a, m, k) => {
       num_subarray_with_sum_k = 1
       last_index = idx_subarray[a[i]]
     }
+    if (!cnt_in_subarray[a[i]]) {
+      cnt_in_subarray[a[i]] = 0
+    }
     cnt_in_subarray[a[i]] += 1
     idx_subarray[k - a[i]] = i
   }
 
   // console.log({ idx_subarray })
+  // console.log({ cnt_in_subarray })
 
   for (let i = m; i < a.length; i++) {
+    // console.log({ caim: cnt_in_subarray[a[i - m]], cnt_in_subarray })
+    // decrement count of first element of sliding window.
     cnt_in_subarray[a[i - m]] -= 1
     if (cnt_in_subarray[a[i - m]] === 0) {
       delete idx_subarray[k - a[i - m]]
     }
 
+    // if current element is a complement found in the sliding window
+    // idx_subarray[a[i] - index of the complement
     if (idx_subarray[a[i]]) {
       num_subarray_with_sum_k += 1
+      // make sure last_index is close to the end of the window
       last_index = Math.max(idx_subarray[a[i]], last_index)
+      // print 'compliment found 1' a[i], last_index
+      console.log('complement found 1: ', a[i], last_index)
     } else if (last_index > i - m) {
+      // ???
       num_subarray_with_sum_k += 1
+      console.log('complement found 2: ', a[i], last_index, i - m)
     }
-
+    if (!cnt_in_subarray[a[i]]) {
+      cnt_in_subarray[a[i]] = 0
+    }
     cnt_in_subarray[a[i]] += 1
     idx_subarray[k - a[i]] = i
   }
