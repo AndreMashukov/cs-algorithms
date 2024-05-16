62f694323c9961b18f3df0b24a1203205c3b2f1d
Andrey Mashukov <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>
2024-05-16 14:00:57 +0800

    changes

--- src/misc/11-may-2024/05-longestPalindrome/longestPalindrome.js
+++ src/misc/11-may-2024/05-longestPalindrome/longestPalindrome.js
@@ -8,6 +8,8 @@
 // Explanation: "aba" is also a valid answer.
 
 const expandAroundCenter = (s, i, j) => {
+  // i - expand to the left
+  // j - expand to the right
   while (i >= 0 && j < s.length && s[i] === s[j]) {
     i -= 1
     j += 1
@@ -22,8 +24,11 @@ const longestPalindrome = (s) => {
   let end = 0
 
   for (let i = 0; i < s.length; i += 1) {
+    // find palindromes of odd length, where there is a single character in the middle
     const len1 = expandAroundCenter(s, i, i)
+    // find palindromes of even length
     const len2 = expandAroundCenter(s, i, i + 1)
+    // takes the maximum of these two lengths, as we're interested in the longest palindromic substring
     const len = Math.max(len1, len2)
 
     if (len > end - start) {
@@ -31,7 +36,7 @@ const longestPalindrome = (s) => {
       end = i + Math.floor(len / 2)
     }
 
-    console.log({ start, end })
+    // console.log({ start, end })
   }
   return s.substring(start, end + 1)
 }
