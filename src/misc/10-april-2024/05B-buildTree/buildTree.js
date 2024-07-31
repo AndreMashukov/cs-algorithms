c29b3152ca9b4dbc5750d00804f6216b85934e41
Andrey Mashukov <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>
2024-07-31 10:27:55 +0800

    changes

--- src/misc/10-april-2024/05B-buildTree/buildTree.js
+++ src/misc/10-april-2024/05B-buildTree/buildTree.js
@@ -1,5 +1,5 @@
+const TreeNode = require('./TreeNode')
 // You are given two integer arrays preorder and inorder.
-
 // preorder is the preorder traversal of a binary tree
 // inorder is the inorder traversal of the same tree
 // Both arrays are of the same size and consist of unique values.
@@ -7,18 +7,6 @@
 
 // Input: preorder = [1,2,3,4], inorder = [2,1,3,4]
 // Output: [1,2,3,null,null,null,4]
-
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     constructor(val = 0, left = null, right = null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
 class Solution {
   /**
    * @param {number[]} preorder
@@ -26,60 +14,23 @@ class Solution {
    * @return {TreeNode}
    */
   buildTree (preorder, inorder) {
-    return this.buildTreeHelper(preorder, inorder, 0, 0, inorder.length - 1)
-  }
-
-  buildTreeHelper (preorder, inorder, preStart, inStart, inEnd) {
-    if (preStart > preorder.length - 1 || inStart > inEnd) {
+    if (!preorder.length || !inorder.length) {
       return null
     }
 
-    const root = new TreeNode(preorder[preStart])
-    let inIndex = 0
-
-    for (let i = inStart; i <= inEnd; i++) {
-      if (inorder[i] === root.val) {
-        inIndex = i
-      }
-    }
-
-    root.left = this.buildTreeHelper(
-      preorder,
-      inorder,
-      preStart + 1,
-      inStart,
-      inIndex - 1
+    const root = new TreeNode(preorder[0])
+    const mid = inorder.indexOf(preorder[0])
+    root.left = this.buildTree(
+      preorder.slice(1, mid + 1),
+      inorder.slice(0, mid)
     )
-    root.right = this.buildTreeHelper(
-      preorder,
-      inorder,
-      preStart + inIndex - inStart + 1,
-      inIndex + 1,
-      inEnd
+    root.right = this.buildTree(
+      preorder.slice(mid + 1),
+      inorder.slice(mid + 1)
     )
-
     return root
   }
 }
 
-// Detailed Steps
-// buildTree Method:
-
-// This is the main method that initiates the tree reconstruction.
-// It calls the helper method buildTreeHelper with initial indices for preorder and inorder arrays.
-// buildTreeHelper Method:
-
-// Base Case: If preStart exceeds the length of the preorder array or inStart exceeds inEnd, return null. This means there are no more nodes to process.
-// Create Root Node: The root node is created using the current value in the preorder array (preorder[preStart]).
-// Find Root in Inorder Array: Loop through the inorder array to find the index of the root node (inIndex). This index splits the inorder array into left and right subtrees.
-// Recursive Calls:
-// Left Subtree: Recursively build the left subtree using the next element in preorder (preStart + 1) and the left part of the inorder array (inStart to inIndex - 1).
-// Right Subtree: Recursively build the right subtree using the appropriate element in preorder (preStart + inIndex - inStart + 1) and the right part of the inorder array (inIndex + 1 to inEnd).
-// Example
-// For preorder = [1, 2, 3, 4] and inorder = [2, 1, 3, 4]:
-
-// First Call: buildTreeHelper(preorder, inorder, 0, 0, 3)
-// Root: 1 (from preorder[0])
-// inIndex of 1 in inorder: 1
-// Left Subtree: buildTreeHelper(preorder, inorder, 1, 0, 0) (for 2)
-// Right Subtree: buildTreeHelper(preorder, inorder, 2, 2, 3) (for 3, 4)
+const solution = new Solution()
+console.log(solution.buildTree([1, 2, 3, 4], [2, 1, 3, 4]))
