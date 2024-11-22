292984c0d4b871ae8416a99d91777c83b72fdace
Andrey Mashukov <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>
2024-11-22 17:46:42 +0800

    changes

--- src/03-TwoPointers/08-findKClosest/index.js
+++ src/03-TwoPointers/08-findKClosest/index.js
@@ -6,6 +6,7 @@
 // in ascending order by the difference between the number and target.
 // Otherwise, sorted in ascending order by number if the difference is same.
 
+// O(Log(N−K))
 class Solution {
   /**
    * @param a: an integer array
@@ -21,7 +22,7 @@ class Solution {
     while (l < r) {
       const m = Math.floor((l + r) / 2)
       // If the left element is closer to the target
-      if (x - arr[m] > arr[m + k] - x) { 
+      if (x - arr[m] > arr[m + k] - x) {
         l = m + 1
       } else {
         r = m
@@ -32,3 +33,39 @@ class Solution {
   }
 }
 console.log(new Solution().kClosestNumbers([1, 2, 3], 3, 2)) // Expected [2, 1, 3]
+
+// Example:
+// Input:
+// Array: [1, 2, 3, 4, 5]
+// Target: 3
+// k: 2
+// Goal:
+// Find the 2 closest numbers to 3 in the array [1, 2, 3, 4, 5].
+
+// Steps:
+// Initialization:
+
+// Left pointer (l): 0
+// Right pointer (r): arr.length - k = 5 - 2 = 3
+// Binary Search:
+
+// First Iteration:
+
+// Calculate middle index (m): Math.floor((0 + 3) / 2) = 1
+// Compare differences:
+// x - arr[m] = 3 - 2 = 1
+// arr[m + k] - x = arr[1 + 2] - 3 = arr[3] - 3 = 4 - 3 = 1
+// Since 1 is not greater than 1, move the right pointer:
+// r = m = 1
+// Second Iteration:
+
+// Calculate middle index (m): Math.floor((0 + 1) / 2) = 0
+// Compare differences:
+// x - arr[m] = 3 - 1 = 2
+// arr[m + k] - x = arr[0 + 2] - 3 = arr[2] - 3 = 3 - 3 = 0
+// Since 2 is greater than 0, move the left pointer:
+// l = m + 1 = 1
+// End of Binary Search:
+
+// The binary search ends with l = 1.
+// The 2 closest numbers starting from index 1 are [2, 3].
--- src/03-TwoPointers/11-strStr/index.js
+++ src/03-TwoPointers/11-strStr/index.js
@@ -1,4 +1,5 @@
 // 28. Find the Index of the First Occurrence in a String
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 // Given two strings needle and haystack, return the index
 // of the first occurrence of needle in haystack,
 // or -1 if needle is not part of haystack.
