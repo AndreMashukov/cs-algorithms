24674a91c405ee1d4d35c8f9fa08dd46240008b9
Andrey Mashukov <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>
2024-11-24 16:51:51 +0800

    changes

--- src/03-TwoPointers/12-nextPermutation/index.js
+++ src/03-TwoPointers/12-nextPermutation/index.js
@@ -1,4 +1,6 @@
 // 31. Next Permutation
+// https://leetcode.com/problems/next-permutation/description/
+// https://www.youtube.com/watch?v=uCst0TJHJvg
 // A permutation of an array of integers is an arrangement of its members
 // into a sequence or linear order.
 
@@ -11,7 +13,7 @@
 // then the next permutation of that array is the permutation that follows
 //  it in the sorted container. If such arrangement is not possible,
 // the array must be rearranged as the lowest possible order
-//  (i.e., sorted in ascending order).
+//  (i.e., sorted in ascending order).d
 
 // For example, the next permutation of arr = [1,2,3] is [1,3,2].
 // Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
