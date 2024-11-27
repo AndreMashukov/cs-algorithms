130369d6de988873e8b4b4f4d260221cadcd2145
Andrey Mashukov <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>
2024-11-27 09:53:45 +0800

    changes

--- src/04-SlidingWindow/06-numberOfSubarrays/numberOfSubarrays.js
+++ src/04-SlidingWindow/06-numberOfSubarrays/numberOfSubarrays.js
@@ -1,4 +1,6 @@
+// 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
 // https://leetcode.com/problems/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/description/
+// https://www.youtube.com/watch?v=D8B4tKxMTnY
 // Given an array of integers arr and two integers k and threshold,
 // return the number of sub-arrays of size k and average greater
 // than or equal to threshold.
