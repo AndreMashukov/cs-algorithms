f4ee7f688acbfd59004e06739e8686b8b08374be
Andrey Mashukov <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>
2024-12-12 08:13:07 +0800

    changes

--- src/06-Backtracking/01-hasPathSum/index.js
+++ src/06-Backtracking/01-hasPathSum/index.js
@@ -1,5 +1,5 @@
 // 112. Path Sum
-
+// https://leetcode.com/problems/path-sum/description/
 // Given the root of a binary tree and an integer targetSum,
 // return true if the tree has a root-to-leaf path
 // such that adding up all the values
