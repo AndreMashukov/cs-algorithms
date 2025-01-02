9b25fe49629e35075e71d305b322b232196d0ac0
Andrey Mashukov <andre.mashukov@gmail.com>
2025-01-02 15:25:57 +0800

    changes

--- src/04-SlidingWindow/12-findSubstring/index.js
+++ src/04-SlidingWindow/12-findSubstring/index.js
@@ -27,31 +27,51 @@
 // It is the concatenation of ["foo","bar"] which is a permutation of words.
 
 const findSubstring = (s, words) => {
+  // Create a frequency map to count occurrences of each word in words
   const freqMap = new Map()
   words.forEach((word) => {
     freqMap.set(word, (freqMap.get(word) || 0) + 1)
   })
 
+  // Length of each word (all words are of the same length)
   const wordLength = words[0].length
 
+  // Result array to store starting indices of concatenated substrings
   const res = []
+
+  // Iterate over the string s
   for (let i = 0; i < s.length - wordLength * words.length + 1; i++) {
+    // Map to keep track of seen words in the current window
     const seen = new Map()
     let j = 0
+
+    // Check if the substring starting at index i is a concatenation of all words
     while (j < words.length) {
+      // Extract a substring of length wordLength
       const word = s.substr(i + j * wordLength, wordLength)
+
+      // If the word is in the frequency map
       if (freqMap.has(word)) {
+        // Increment the count of the word in the seen map
         seen.set(word, (seen.get(word) || 0) + 1)
+
+        // If the word count exceeds the frequency in the original words array, break
         if (seen.get(word) > freqMap.get(word)) {
           break
         }
       } else {
+        // If the word is not in the frequency map, break
         break
       }
       j++
     }
+
+    // If all words are matched, add the starting index to the result
     if (j === words.length) {
       res.push(i)
     }
   }
+
+  // Return the result array
+  return res
 }
