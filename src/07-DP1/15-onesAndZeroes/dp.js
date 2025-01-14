a012be608b6eb021f4c3f9c7857f0cc7c6046e58
Andrey Mashukov <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>
2025-01-14 20:28:42 +0800

    changes

--- src/07-DP1/15-onesAndZeroes/dp.js
+++ src/07-DP1/15-onesAndZeroes/dp.js
@@ -1,5 +1,6 @@
 // 474. Ones and Zeroes
 // https://leetcode.com/problems/ones-and-zeroes/description/
+// https://www.youtube.com/watch?v=miZ3qV04b1g
 // You are given an array of binary strings strs
 // and two integers m and n.
 
@@ -51,3 +52,60 @@ const findMaxFormDp = function (strs, m, n) {
   // Return the maximum size of the subset with at most m zeros and n ones
   return dp[m][n]
 }
+
+// strs = ["1", "0", "10"]
+// m = 1 (max zeros allowed)
+// n = 1 (max ones allowed)
+
+// Initial dp[m+1][n+1]:
+//        0_1s  1_1s
+// 0_0s    0     0
+// 1_0s    0     0
+
+// After "1":
+//        0_1s  1_1s
+// 0_0s    0     1    // Can take "1" if we have capacity for 1 one
+// 1_0s    0     1
+
+// After "0":
+//        0_1s  1_1s
+// 0_0s    0     1
+// 1_0s    1     1    // Can take "0" if we have capacity for 1 zero
+
+// After "10":
+//        0_1s  1_1s
+// 0_0s    0     1
+// 1_0s    1     1    // Can't take "10" as it needs 1 zero AND 1 one
+
+// strs = ["1", "0", "10"]
+// m = 1 (max zeros)
+// n = 1 (max ones)
+
+// dp[1][1] = 1 means:
+// - Using at most 1 zero AND at most 1 one
+// - We can form a subset of size 1
+// - Valid subsets: ["1"] or ["0"]
+// - Can't include "10" as it needs both zero and one
+
+// AndreMashukov
+// what dp[m][n] actually means? what question does it answer?
+
+// GitHub Copilot
+// dp[m][n] Meaning:
+// dp[m][n] represents the maximum number of strings that can be formed using:
+
+// at most m zeros
+// at most n ones
+// Example Breakdown:
+// strs = ["1", "0", "10"]
+// m = 1 (max zeros)
+// n = 1 (max ones)
+
+// dp[1][1] = 1 means:
+// - Using at most 1 zero AND at most 1 one
+// - We can form a subset of size 1
+// - Valid subsets: ["1"] or ["0"]
+// - Can't include "10" as it needs both zero and one
+// Questions dp[m][n] Answers:
+// "What's the largest subset size possible?"
+// "How many strings can I pick while staying within m zeros and n ones?"
