952de711f81a0edb30e3a8551eb36883e0c50efb
Andrey Mashukov <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>
2025-06-11 14:47:08 +0800

    changes

--- src/09-DP3/06-uniquePaths2/dp.js
+++ src/09-DP3/06-uniquePaths2/dp.js
@@ -1,9 +1,12 @@
 // 63. Unique Paths II
 // https://leetcode.com/problems/unique-paths-ii/description/?envType=problem-list-v2&envId=dynamic-programming
 // Problem Description:
-// You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]).
-// The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
-// An obstacle and space are marked as 1 and 0 respectively in grid. A path that the robot takes cannot include any square that is an obstacle.
+// You are given an m x n integer array grid. There is a robot initially located at the top-left corner
+// (i.e., grid[0][0]).
+// The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). 
+// The robot can only move either down or right at any point in time.
+// An obstacle and space are marked as 1 and 0 respectively in grid. 
+// \A path that the robot takes cannot include any square that is an obstacle.
 // Return the number of possible unique paths that the robot can take to reach the bottom-right corner.
 //
 // Example 1:
--- src/09-DP3/07-minimumPathSum/dp.js
+++ src/09-DP3/07-minimumPathSum/dp.js
@@ -27,36 +27,38 @@
  * @return {number} - Minimum sum from top-left to bottom-right
  */
 function minPathSum(grid) {
-    // Get grid dimensions
-    const m = grid.length;
-    const n = grid[0].length;
-    
-    // Create DP table where dp[i][j] = minimum sum to reach cell (i,j)
-    const dp = Array(m).fill(null).map(() => Array(n).fill(0));
-    
-    // Initialize starting position
-    dp[0][0] = grid[0][0];
-    
-    // Fill first row: can only come from left
+  // Get grid dimensions
+  const m = grid.length;
+  const n = grid[0].length;
+
+  // Create DP table where dp[i][j] = minimum sum to reach cell (i,j)
+  const dp = Array(m)
+    .fill(null)
+    .map(() => Array(n).fill(0));
+
+  // Initialize starting position
+  dp[0][0] = grid[0][0];
+
+  // Fill first row: can only come from left
+  for (let j = 1; j < n; j++) {
+    dp[0][j] = dp[0][j - 1] + grid[0][j];
+  }
+
+  // Fill first column: can only come from above
+  for (let i = 1; i < m; i++) {
+    dp[i][0] = dp[i - 1][0] + grid[i][0];
+  }
+
+  // Fill rest of DP table
+  for (let i = 1; i < m; i++) {
     for (let j = 1; j < n; j++) {
-        dp[0][j] = dp[0][j - 1] + grid[0][j];
+      // Minimum sum = current cell + min(from above, from left)
+      dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
     }
-    
-    // Fill first column: can only come from above
-    for (let i = 1; i < m; i++) {
-        dp[i][0] = dp[i - 1][0] + grid[i][0];
-    }
-    
-    // Fill rest of DP table
-    for (let i = 1; i < m; i++) {
-        for (let j = 1; j < n; j++) {
-            // Minimum sum = current cell + min(from above, from left)
-            dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
-        }
-    }
-    
-    // Return minimum sum to reach bottom-right corner
-    return dp[m - 1][n - 1];
+  }
+
+  // Return minimum sum to reach bottom-right corner
+  return dp[m - 1][n - 1];
 }
 
 /**
@@ -66,35 +68,37 @@ function minPathSum(grid) {
  * @return {number} - Minimum path sum
  */
 function minPathSumOptimized(grid) {
-    const m = grid.length;
-    const n = grid[0].length;
-    
-    // Use 1D DP array representing current row
-    const dp = Array(n).fill(0);
-    
-    // Initialize first element
-    dp[0] = grid[0][0];
-    
-    // Fill first row
+  const m = grid.length;
+  const n = grid[0].length;
+
+  // Use 1D DP array representing current row
+  const dp = Array(n).fill(0);
+
+  // Initialize first element
+  dp[0] = grid[0][0];
+
+  // Fill first row
+  for (let j = 1; j < n; j++) {
+    dp[j] = dp[j - 1] + grid[0][j];
+  }
+  // [[1,3,1],[1,5,1],[4,2,1]]
+  // console.log(dp); [ 1, 4, 5 ]
+
+  // Process remaining rows
+  for (let i = 1; i < m; i++) {
+    // Update first column (can only come from above)
+    dp[0] += grid[i][0];
+
+    // Update rest of columns
     for (let j = 1; j < n; j++) {
-        dp[j] = dp[j - 1] + grid[0][j];
+      // dp[j] represents value from above (previous row)
+      // dp[j-1] represents value from left (current row)
+      dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1]);
     }
-    
-    // Process remaining rows
-    for (let i = 1; i < m; i++) {
-        // Update first column (can only come from above)
-        dp[0] += grid[i][0];
-        
-        // Update rest of columns
-        for (let j = 1; j < n; j++) {
-            // dp[j] represents value from above (previous row)
-            // dp[j-1] represents value from left (current row)
-            dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1]);
-        }
-    }
-    
-    // Return minimum sum to reach bottom-right corner
-    return dp[n - 1];
+  }
+
+  // Return minimum sum to reach bottom-right corner
+  return dp[n - 1];
 }
 
 /**
@@ -104,29 +108,29 @@ function minPathSumOptimized(grid) {
  * @return {number} - Minimum path sum
  */
 function minPathSumInPlace(grid) {
-    const m = grid.length;
-    const n = grid[0].length;
-    
-    // Fill first row: accumulate sums from left
+  const m = grid.length;
+  const n = grid[0].length;
+
+  // Fill first row: accumulate sums from left
+  for (let j = 1; j < n; j++) {
+    grid[0][j] += grid[0][j - 1];
+  }
+
+  // Fill first column: accumulate sums from above
+  for (let i = 1; i < m; i++) {
+    grid[i][0] += grid[i - 1][0];
+  }
+
+  // Fill rest of grid
+  for (let i = 1; i < m; i++) {
     for (let j = 1; j < n; j++) {
-        grid[0][j] += grid[0][j - 1];
+      // Current cell + minimum of (above, left)
+      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
     }
-    
-    // Fill first column: accumulate sums from above
-    for (let i = 1; i < m; i++) {
-        grid[i][0] += grid[i - 1][0];
-    }
-    
-    // Fill rest of grid
-    for (let i = 1; i < m; i++) {
-        for (let j = 1; j < n; j++) {
-            // Current cell + minimum of (above, left)
-            grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-        }
-    }
-    
-    // Return result from bottom-right corner
-    return grid[m - 1][n - 1];
+  }
+
+  // Return result from bottom-right corner
+  return grid[m - 1][n - 1];
 }
 
 /**
@@ -135,40 +139,48 @@ function minPathSumInPlace(grid) {
  * @return {number} - Minimum path sum
  */
 function minPathSumExplicit(grid) {
-    const m = grid.length;
-    const n = grid[0].length;
-    
-    // Create DP table
-    const dp = Array(m).fill(null).map(() => Array(n).fill(Infinity));
-    
-    // Base case
-    dp[0][0] = grid[0][0];
-    
-    // Fill DP table
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (i === 0 && j === 0) continue; // Skip base case
-            
-            let minSum = Infinity;
-            
-            // Can come from above
-            if (i > 0) {
-                minSum = Math.min(minSum, dp[i - 1][j]);
-            }
-            
-            // Can come from left
-            if (j > 0) {
-                minSum = Math.min(minSum, dp[i][j - 1]);
-            }
-            
-            // Update current cell
-            dp[i][j] = grid[i][j] + minSum;
-        }
+  const m = grid.length;
+  const n = grid[0].length;
+
+  // Create DP table
+  const dp = Array(m)
+    .fill(null)
+    .map(() => Array(n).fill(Infinity));
+
+  // Base case
+  dp[0][0] = grid[0][0];
+
+  // Fill DP table
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i === 0 && j === 0) continue; // Skip base case
+
+      let minSum = Infinity;
+
+      // Can come from above
+      if (i > 0) {
+        minSum = Math.min(minSum, dp[i - 1][j]);
+      }
+
+      // Can come from left
+      if (j > 0) {
+        minSum = Math.min(minSum, dp[i][j - 1]);
+      }
+
+      // Update current cell
+      dp[i][j] = grid[i][j] + minSum;
     }
-    
-    return dp[m - 1][n - 1];
+  }
+
+  return dp[m - 1][n - 1];
 }
 
 // Example usage:
-// console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]])); // Output: 7
+console.log(
+  minPathSumOptimized([
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1]
+  ])
+); // Output: 7
 // console.log(minPathSumOptimized([[1,2,3],[4,5,6]])); // Output: 12
