f877e5098aaa6205f57d65b198251ba5e937f830
Andrey Mashukov <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>
2025-06-24 09:23:57 +0800

    changes

--- src/09-DP3/11-pascalsTriangle/recursive.js
+++ src/09-DP3/11-pascalsTriangle/recursive.js
@@ -1,5 +1,6 @@
 // 118. Pascal's Triangle
 // https://leetcode.com/problems/pascals-triangle/description/?envType=problem-list-v2&envId=dynamic-programming
+// https://www.youtube.com/watch?v=nPVEaB3AjUM
 // Problem Description:
 // Given an integer numRows, return the first numRows of Pascal's triangle.
 // In Pascal's triangle, each number is the sum of the two numbers directly above it.
