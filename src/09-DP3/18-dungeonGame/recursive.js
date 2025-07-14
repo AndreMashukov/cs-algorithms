f6578a14f76c7dfde53c46377a3d2f79544fad03
Andrey Mashukov <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>
2025-07-14 18:32:47 +0800

    changes

--- src/09-DP3/18-dungeonGame/recursive.js
+++ src/09-DP3/18-dungeonGame/recursive.js
@@ -5,10 +5,12 @@
 // The dungeon consists of m x n rooms laid out in a 2D grid. Our valiant knight was initially positioned
 // in the top-left room and must fight his way through the dungeon to rescue the princess.
 //
-// The knight has an initial health point represented by a positive integer. If at any point his health
+// The knight has an initial health point represented by a positive integer. 
+// If at any point his health
 // point drops to 0 or below, he dies immediately.
 //
-// Some of the rooms are guarded by demons (represented by negative integers), so the knight loses health
+// Some of the rooms are guarded by demons (represented by negative integers), 
+// so the knight loses health
 // upon entering these rooms; other rooms are either empty (represented as 0) or contain magic orbs that
 // increase the knight's health (represented by positive integers).
 //
@@ -22,7 +24,8 @@
 // Example 1:
 // Input: dungeon = [[-2,-3,3],[-5,-10,1],[10,30,-5]]
 // Output: 7
-// Explanation: The initial health of the knight must be at least 7 if he follows the optimal path: RIGHT -> RIGHT -> DOWN -> DOWN.
+// Explanation: The initial health of the knight must be at least 7 
+// if he follows the optimal path: RIGHT -> RIGHT -> DOWN -> DOWN.
 //
 // Example 2:
 // Input: dungeon = [[0]]
