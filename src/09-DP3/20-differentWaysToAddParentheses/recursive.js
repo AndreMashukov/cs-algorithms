2d66d9c4b4e2a23bf6e0d7f11d663d77d3e6ab7b
Andrey Mashukov <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>
2025-07-17 21:18:55 +0800

    changes

--- src/09-DP3/20-differentWaysToAddParentheses/recursive.js
+++ src/09-DP3/20-differentWaysToAddParentheses/recursive.js
@@ -1,5 +1,6 @@
 // 241. Different Ways to Add Parentheses
 // https://leetcode.com/problems/different-ways-to-add-parentheses/description/
+// https://www.youtube.com/watch?v=cykVFFm5D3s
 // Problem Description:
 // Given a string expression of numbers and operators, return all possible results from computing
 // all the different possible ways to group numbers and operators. You may return the answer in any order.
