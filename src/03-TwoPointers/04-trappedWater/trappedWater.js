c0eb9d87054d3aafd13a2360d385678564becb96
Andrey Mashukov <andre.mashukov@gmail.com>
2026-04-20 11:56:49 +0800

    changes

--- src/03-TwoPointers/04-trappedWater/trappedWater.js
+++ src/03-TwoPointers/04-trappedWater/trappedWater.js
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/trapping-rain-water/description/
 // You are given an array non-negative integers heights which represent an elevation map.
 // Each value heights[i] represents the height of a bar, which has a width of 1.
 // Return the maximum area of water that can be trapped between the bars.
