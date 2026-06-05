ef5011979f4038bd0295d6e7eb2f1bec7dfb554c
Andrey Mashukov <andre.mashukov@gmail.com>
2026-06-05 09:02:42 +0800

    changes

---
+++ src/09-DP3/16-buySellStock3/optimal.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+  let buy1 = Infinity, sell1 = 0
+  let buy2 = Infinity, sell2 = 0
+
+  for (let price of prices) {
+    buy1 = Math.min(buy1, price);
+    sell1 = Math.max(sell1, price - buy1)
+    buy2 = Math.min(buy2, price - sell1);
+    sell2 = Math.max(sell2, price - buy2);
+  }
+
+  return sell2
+};
